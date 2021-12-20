3b1f6e9dba78212a75a4514235a9ff1fc540ea09
김의진 <kpasd002@gmail.com>
2021-12-20 17:01:27 +0900

    이미지 추가 , home , profile 페이지 코드 변경

--- lib/page/profile.dart
+++ lib/page/profile.dart
@@ -4,8 +4,23 @@ class Profile extends StatelessWidget {
   const Profile({Key? key}) : super(key: key); // 무슨 코드인지 확인 해 볼 것.
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("마이 잇츠"),
+    return ListView(
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      children: const <Widget>[
+        ListTile(
+          leading: Icon(Icons.eleven_mp),
+          title: Text("ㅇㄹㅇㄹ"),
+        ),
+        ListTile(
+          leading: Icon(Icons.eleven_mp),
+          title: Text("ㅇㄹㅇㄹ"),
+        ),
+        ListTile(
+          leading: Icon(Icons.eleven_mp),
+          title: Text("ㅇㄹㅇㄹ"),
+        ),
+      ],
     );
   }
 }

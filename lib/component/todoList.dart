4cc4b7c0eb031965b867461a56e1941c37eb7e84
Jin-devST <kpasd002@gmail.com>
2021-12-24 15:01:33 +0900

    파이어베이스 클라우드 디비 설정 및 적용

--- lib/component/todoList.dart
+++ lib/component/todoList.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:date_format/date_format.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 // import 'package:date'
@@ -11,34 +13,36 @@ class TodoList extends StatefulWidget {
 
 class Todo {
   String title;
-  bool isDone = false;
+  bool isDone;
   String dateTime = formatDate(DateTime.now(), [hh, ':', nn, ':', ss, ' ', am]);
 
-  Todo(this.title); // 생성자 생성
+  Todo(this.title, {this.isDone = false}); // 생성자 생성
+// Todo(this.title ,@require xxx , ? 가변인자 취급); // 생성자 생성
 }
 
 class _TodoListState extends State<TodoList> {
-  final _items = <Todo>[];
   final _todoController = TextEditingController();
+  final CollectionReference todoCollectionReference = FirebaseFirestore.instance.collection("todo");
 
   void _addTodo(Todo todo) {
-    setState(() {
-      _items.add(todo);
-      _todoController.text = "";
+    todoCollectionReference.add({
+      'title': todo.title,
+      'isDone': todo.isDone,
+      'dateTime': todo.dateTime
     });
+    _todoController.text = "";
   }
 
-  void _deleteTodo(Todo todo) {
-    if (!_items.contains(todo)) return;
-    setState(() {
-      _items.remove(todo);
-    });
+  void _deleteTodo(DocumentSnapshot doc) {
+    // if (!_items.contains(doc)) return;
+    // setState(() {
+    //   _items.remove(doc);
+    // });
+    todoCollectionReference.doc(doc.id).delete();
   }
 
-  void _toggleTodo(Todo todo) {
-    setState(() {
-      todo.isDone = !todo.isDone;
-    });
+  void _toggleTodo(DocumentSnapshot doc) {
+    todoCollectionReference.doc(doc.id).update({'isDone' : !doc['isDone']});
   }
 
   @override
@@ -52,28 +56,28 @@ class _TodoListState extends State<TodoList> {
     super.dispose();
   }
 
-  Widget _buildTodoListItem(Todo todo) {
+  Widget _buildTodoListItem(DocumentSnapshot doc) {
+    final todo = Todo(doc['title'],isDone: doc['isDone']);
     return ListTile(
-      title: Expanded(
-        child: Row(
-          children: [
-            Text(todo.title,
-                style: todo.isDone
-                    ? const TextStyle(
-                  decoration: TextDecoration.lineThrough,
-                  fontStyle: FontStyle.italic,
-                )
-                    : null),
-            Text(todo.dateTime),
-          ],
-        ),
+      title: Row(
+        children: [
+          Text(todo.title,
+              style: todo.isDone
+                  ? const TextStyle(
+                      decoration: TextDecoration.lineThrough,
+                      fontStyle: FontStyle.italic,
+                    )
+                  : null),
+          Text(todo.dateTime),
+        ],
       ),
-      trailing:
-      IconButton(onPressed: () {
-        _deleteTodo(todo);
-      }, icon: const Icon(Icons.delete_forever)),
+      trailing: IconButton(
+          onPressed: () {
+            _deleteTodo(doc);
+          },
+          icon: const Icon(Icons.delete_forever)),
       onTap: () {
-        _toggleTodo(todo);
+        _toggleTodo(doc);
       },
     );
   }
@@ -107,14 +111,22 @@ class _TodoListState extends State<TodoList> {
                 ),
               ],
             ),
-            Expanded(
-              child: ListView(
-                // listview 위젯은 column 위젯의 child가 될때 expand으로 감싸줘야 정상적으로 작동함.
-                shrinkWrap: true,
-                scrollDirection: Axis.vertical,
-                children:
-                _items.map((todo) => _buildTodoListItem(todo)).toList(),
-              ),
+            StreamBuilder<QuerySnapshot>(
+              stream: todoCollectionReference.snapshots(),
+              builder: (context, snapshot) {
+                if(!snapshot.hasData){
+                  return CircularProgressIndicator();
+                }
+                final documents = snapshot.data!.docs;
+                return Expanded(
+                  child: ListView(
+                    // listview 위젯은 column 위젯의 child가 될때 expand으로 감싸줘야 정상적으로 작동함.
+                    shrinkWrap: true,
+                    scrollDirection: Axis.vertical,
+                    children: documents.map((doc) => _buildTodoListItem(doc)).toList(),
+                  ),
+                );
+              }
             )
           ],
         ),
